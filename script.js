ef018cb713c7bfdd74721d61e40cf734c3b613cd
Megha Banerjee <megha.banerjee07@gmail.com>
2020-12-28 16:25:58 +0530

    removed console.log

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ element.addEventListener('submit', event => {
 let numberGuessedComputer;
 function rangeDefined(){
     let num1 = document.getElementById('num1').value;
-    console.log(num1);
-    console.log(typeof(num1));
 
     let num2 = document.getElementById('num2').value;
     console.log(num2);
@@ -16,10 +14,7 @@ function rangeDefined(){
         alert('Please enter the range of numbers for the game.');
     }else{
         let a = Math.random();
-    
         numberGuessedComputer = Math.floor(Math.random() * (parseInt(num2) - parseInt(num1) + 1)) + parseInt(num1);
-        console.log(numberGuessedComputer);
-
         document.querySelector('.play').style.display = 'block';
     }
 }
@@ -28,8 +23,7 @@ let counter = 0;
 function userGuessedNumber(){
 
     let numberGuessedUser = document.getElementById('selectedNum').value;
-    console.log(numberGuessedUser);
-
+   
     if(parseInt(numberGuessedUser) ===  parseInt(numberGuessedComputer)){
         document.querySelector('.won').style.display = 'block';
         document.querySelector('.more').style.display = 'none';
